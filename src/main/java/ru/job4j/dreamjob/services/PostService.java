199b9b5323efa1f7605391be7bb2fc4b54ff134d
Maxim Tsurkanov <max86ts@gmail.com>
2022-06-08 22:32:04 +0300

    1. Слоеная архитектура. [#504851 #310545]

---
+++ src/main/java/ru/job4j/dreamjob/services/PostService.java
@@ -0,0 +1,37 @@
+package ru.job4j.dreamjob.services;
+
+import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.store.PostStore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostService {
+
+    private final static PostService INST = new PostService();
+    private final PostStore store = PostStore.instOf();
+
+    private PostService() {
+
+    }
+
+    public static PostService instOf() {
+        return INST;
+    }
+
+    public List<Post> findAll() {
+        return new ArrayList<>(store.findAll());
+    }
+
+    public void add(Post post) {
+        store.add(post);
+    }
+
+    public Post findById(int id) {
+        return store.findById(id);
+    }
+
+    public void update(Post post) {
+        store.update(post);
+    }
+}
